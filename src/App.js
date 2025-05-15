5651cc3c48ce018003ed0acad016e0df4014fa90
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2025-05-15 20:28:27 +0200

    style

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Todos from './components/Todos';
 
 function App() {
   return (
-    <div className="App">
-      <div className='container-fluid cont'>
-        <header className="App-header">
-          <h1>Todo lista</h1>
-        
+    <div className="App cont d-flex flex-column min-vh-100">
+        <header className="App-header w-100 bg-light">
+       
+            <h1>Todo lista</h1>
         </header>
-        <div className='section container'>
+
+        <div className='section container flex-grow-1'>
           <Todos/>
 
 
         </div>
-        <footer className='text-center bg-light'>Szalóki Klaudia</footer>
+        <footer className='text-center bg-light mt-auto'>Szalóki Klaudia</footer>
 
-      </div>
 
     </div>
   );
